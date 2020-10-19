f3b73fddfcefce05338a3ce09344498561c1beef
Timeiswax <58233833+Timeiswax@users.noreply.github.com>
2020-10-19 11:34:57 -0400

    goddamn capitalization for browser compatibility?

--- public/static/data/horizVids.js
+++ public/static/data/horizVids.js
@@ -1,9 +1,9 @@
 const horizVids = [
-    'static/mp4/pre-comp horiz.mp4',
-    'static/mp4/pre-comp horiz_1.mp4',
-    'static/mp4/pre-comp horiz_2.mp4',
-    'static/mp4/pre-comp horiz_3.mp4',
-    'static/mp4/pre-comp horiz_4.mp4',
+    'static/mp4/Pre-comp Horiz.mp4',
+    'static/mp4/Pre-comp Horiz_1.mp4',
+    'static/mp4/Pre-comp Horiz_2.mp4',
+    'static/mp4/Pre-comp Horiz_3.mp4',
+    'static/mp4/Pre-comp Horiz_4.mp4',
 ]
 
 
