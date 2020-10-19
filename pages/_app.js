444f86a6e773d046efa895c8fb919525a1722bc0
Timeiswax <58233833+Timeiswax@users.noreply.github.com>
2020-10-18 20:16:23 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,13 +3,16 @@ import { useRouter } from 'next/router'
 import { CSSTransition, TransitionGroup} from 'react-transition-group'
 import BlogContext from './contexts/blogContextProvider'
 
-const dirs = ['swipe-u','swipe-d','swipe-l','swipe-r']
+const dirs1 = ['swipe-u','swipe-d']
+const dirs2 = ['swipe-l','swipe-r']
+const dirs = [dirs1, dirs2]
 //const dirs = ['swipe-l', 'swipe-r']
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
-const len = dirs.length
-console.log(dirs[getRandomInt(len)])
+const len1 = dirs.length
+const len2 = dirs1.length
+const the_int = getRandomInt(len1)
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter()
@@ -19,7 +22,7 @@ function MyApp({ Component, pageProps }) {
       appear={true}
       key={router.pathname}
       timeout={1000}
-      classNames={dirs[getRandomInt(len)]}
+      classNames={dirs[getRandomInt(the_int)][getRandomInt(len2)]}
       >
         <BlogContext>
           <Component {...pageProps} />
